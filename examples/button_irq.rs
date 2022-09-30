eef31e9c64597488534997658aa926bc3fea6477
Javier Cardona <javier.cardona@gmail.com>
2022-09-30 16:29:37 +0100

    Update button_irq to lightnote

--- examples/button_irq.rs
+++ examples/button_irq.rs
@@ -19,7 +19,7 @@ use stm32l0xx_hal::{
     syscfg::SYSCFG,
 };
 
-static LED: Mutex<RefCell<Option<gpiob::PB6<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+static LED: Mutex<RefCell<Option<gpioa::PA7<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -28,15 +28,16 @@ fn main() -> ! {
     // Configure the clock.
     let mut rcc = dp.RCC.freeze(Config::hsi16());
 
-    // Acquire the GPIOB peripheral. This also enables the clock for GPIOB in
-    // the RCC register.
+    // Acquire the GPIOA, GPIOB peripheral. This also enables the clock for
+    // GPIOs in the RCC register.
+    let gpioa = dp.GPIOA.split(&mut rcc);
     let gpiob = dp.GPIOB.split(&mut rcc);
 
     // Configure PB6 as output.
-    let led = gpiob.pb6.into_push_pull_output();
+    let led = gpioa.pa7.into_push_pull_output();
 
-    // Configure PB2 as input.
-    let button = gpiob.pb2.into_pull_up_input();
+    // Configure PB12 as input.
+    let button = gpiob.pb12.into_pull_up_input();
 
     let mut syscfg = SYSCFG::new(dp.SYSCFG, &mut rcc);
     let mut exti = Exti::new(dp.EXTI);
@@ -53,7 +54,7 @@ fn main() -> ! {
 
     // Enable the external interrupt in the NVIC.
     unsafe {
-        NVIC::unmask(Interrupt::EXTI2_3);
+        NVIC::unmask(Interrupt::EXTI4_15);
     }
 
     loop {
@@ -62,23 +63,14 @@ fn main() -> ! {
 }
 
 #[interrupt]
-fn EXTI2_3() {
-    // Keep the LED state.
-    static mut STATE: bool = false;
-
+fn EXTI4_15() {
     cortex_m::interrupt::free(|cs| {
         // Clear the interrupt flag.
         Exti::unpend(GpioLine::from_raw_line(2).unwrap());
 
         // Change the LED state on each interrupt.
         if let Some(ref mut led) = LED.borrow(cs).borrow_mut().deref_mut() {
-            if *STATE {
-                led.set_low().unwrap();
-                *STATE = false;
-            } else {
-                led.set_high().unwrap();
-                *STATE = true;
-            }
+            led.toggle().unwrap();
         }
     });
 }
