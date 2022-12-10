9f563efe56ccbedc74d4800bc787f21bc3bb2011
Javier Cardona <javier.cardona@gmail.com>
2022-12-10 18:50:44 +0000

    Low power testing on lightnote

---
+++ examples/pwr_lgtnt.rs
@@ -0,0 +1,146 @@
+#![no_main]
+#![no_std]
+
+extern crate panic_halt;
+
+use cortex_m::asm;
+use cortex_m_rt::entry;
+use stm32l0xx_hal::{
+    exti::{ConfigurableLine, Exti, TriggerEdge},
+    gpio::{Output, Pin, PushPull},
+    pac,
+    prelude::*,
+    pwr::{self, PWR},
+    rcc,
+    rtc::{self, ClockSource, Rtc},
+    spi::MODE_0,
+};
+
+
+use spi_memory::series25::Flash;
+
+#[entry]
+fn main() -> ! {
+    let cp = pac::CorePeripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut scb = cp.SCB;
+    let mut rcc = dp.RCC.freeze(rcc::Config::msi(rcc::MSIRange::Range0));
+    let mut exti = Exti::new(dp.EXTI);
+    let mut pwr = PWR::new(dp.PWR, &mut rcc);
+    let gpioa = dp.GPIOA.split(&mut rcc);
+    let gpiob = dp.GPIOB.split(&mut rcc);
+
+    let mut supercap_read_en = gpioa.pa4.into_push_pull_output().downgrade();
+    let mut led = gpioa.pa7.into_push_pull_output().downgrade();
+    let sck = gpiob.pb3;
+    let miso = gpiob.pb4;
+    let mosi = gpiob.pb5;
+    let cs_flash = gpiob.pb6.into_push_pull_output();
+
+    supercap_read_en.set_low().unwrap();
+
+    let spi = dp
+        .SPI1
+        .spi((sck, miso, mosi), MODE_0, 2_000_000.Hz(), &mut rcc);
+
+    // let mut spi2 = p
+    //     .SPI2
+    //     .spi((sck2, miso2, mosi2), MODE_3, 2_000_000.Hz(), &mut rcc);
+
+    let flash = Flash::init(spi, cs_flash);
+    let mut flash = flash.unwrap();
+    flash.wakeup().unwrap();
+    flash.sleep().unwrap();
+
+    // Initialize RTC
+    let mut rtc = Rtc::new(dp.RTC, &mut rcc, &pwr, ClockSource::LSI, None).unwrap();
+
+    // Enable interrupts
+    let exti_line = ConfigurableLine::RtcWakeup;
+    rtc.enable_interrupts(rtc::Interrupts {
+        wakeup_timer: true,
+        ..rtc::Interrupts::default()
+    });
+    exti.listen_configurable(exti_line, TriggerEdge::Rising);
+
+    let mut timer = rtc.wakeup_timer();
+
+    // Blink twice to signal the start of the program
+    blink(&mut led);
+    blink(&mut led);
+
+    // // 5 seconds of regular run mode
+    // timer.start(5u32);
+    // while let Err(nb::Error::WouldBlock) = timer.wait() {}
+    // Exti::unpend(exti_line);
+    // NVIC::unpend(pac::Interrupt::RTC);
+
+    // blink(&mut led);
+
+    // // 5 seconds of low-power run mode
+    // pwr.enter_low_power_run_mode(rcc.clocks);
+    // while let Err(nb::Error::WouldBlock) = timer.wait() {}
+    // pwr.exit_low_power_run_mode();
+    // Exti::unpend(exti_line);
+    // NVIC::unpend(pac::Interrupt::RTC);
+
+    // blink(&mut led);
+
+    // // 5 seconds of sleep mode
+    // exti.wait_for_irq(exti_line, pwr.sleep_mode(&mut scb));
+    // timer.wait().unwrap(); // returns immediately; we just got the interrupt
+
+    // blink(&mut led);
+
+    // // 5 seconds of low-power sleep mode
+    // exti.wait_for_irq(exti_line, pwr.low_power_sleep_mode(&mut scb, &mut rcc));
+    // timer.wait().unwrap(); // returns immediately; we just got the interrupt
+
+    // blink(&mut led);
+
+    // 20 seconds of stop mode
+    timer.start(20u32);
+    exti.wait_for_irq(
+        exti_line,
+        pwr.stop_mode(
+            &mut scb,
+            &mut rcc,
+            pwr::StopModeConfig {
+                ultra_low_power: true,
+            },
+        ),
+    );
+    timer.wait().unwrap(); // returns immediately; we just got the interrupt
+
+    // signal that we are entering standby mode
+    blink(&mut led);
+    blink(&mut led);
+    blink(&mut led);
+
+    // 5 seconds of standby mode
+    cortex_m::peripheral::NVIC::unpend(pac::Interrupt::RTC);
+    exti.wait_for_irq(exti_line, pwr.standby_mode(&mut scb));
+
+    // The microcontroller resets after leaving standby mode. We should never
+    // reach this point.
+    loop {
+        blink(&mut led);
+    }
+}
+
+fn blink(led: &mut Pin<Output<PushPull>>) {
+    led.set_high().unwrap();
+    delay();
+    led.set_low().unwrap();
+    delay();
+}
+
+fn delay() {
+    // We can't use `Delay`, as that requires a frequency of at least one MHz.
+    // Given our clock selection, the following loop should give us a nice delay
+    // when compiled in release mode.
+    for _ in 0..1_000 {
+        asm::nop()
+    }
+}
