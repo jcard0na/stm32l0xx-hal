f1f852b99d1f9005e044debcc2a7718eb7b1e78c
Javier Cardona <javier.cardona@gmail.com>
2023-01-06 21:45:19 +0000

    Disable gpio clocks, set to analog

--- examples/pwr.rs
+++ examples/pwr.rs
@@ -3,7 +3,7 @@
 
 extern crate panic_halt;
 
-use cortex_m::{asm, peripheral::NVIC};
+use cortex_m::asm;
 use cortex_m_rt::entry;
 use stm32l0xx_hal::{
     exti::{ConfigurableLine, Exti, TriggerEdge},
@@ -11,13 +11,10 @@ use stm32l0xx_hal::{
     pac,
     prelude::*,
     pwr::{self, PWR},
-    rcc,
+    rcc::{self, Rcc},
     rtc::{self, ClockSource, Rtc},
-    spi::MODE_0,
 };
 
-
-use spi_memory::series25::Flash;
 
 #[entry]
 fn main() -> ! {
@@ -30,12 +27,8 @@ fn main() -> ! {
     let mut pwr = PWR::new(dp.PWR, &mut rcc);
     let gpioa = dp.GPIOA.split(&mut rcc);
     let gpiob = dp.GPIOB.split(&mut rcc);
-
-    let mut supercap_read_en = gpioa.pa4.into_push_pull_output().downgrade();
     let mut rled = gpioa.pa5.into_push_pull_output().downgrade();
     let mut gled = gpiob.pb4.into_push_pull_output().downgrade();
-
-    supercap_read_en.set_low().unwrap();
 
     // Initialize RTC
     let mut rtc = Rtc::new(dp.RTC, &mut rcc, &pwr, ClockSource::LSI, None).unwrap();
@@ -54,37 +47,9 @@ fn main() -> ! {
     blink(&mut rled);
     blink(&mut gled);
 
-    // // 5 seconds of regular run mode
-    // timer.start(5u32);
-    // while let Err(nb::Error::WouldBlock) = timer.wait() {}
-    // Exti::unpend(exti_line);
-    // NVIC::unpend(pac::Interrupt::RTC);
-
-    // blink(&mut led);
-
-    // // 5 seconds of low-power run mode
-    // pwr.enter_low_power_run_mode(rcc.clocks);
-    // while let Err(nb::Error::WouldBlock) = timer.wait() {}
-    // pwr.exit_low_power_run_mode();
-    // Exti::unpend(exti_line);
-    // NVIC::unpend(pac::Interrupt::RTC);
-
-    // blink(&mut led);
-
-    // // 5 seconds of sleep mode
-    // exti.wait_for_irq(exti_line, pwr.sleep_mode(&mut scb));
-    // timer.wait().unwrap(); // returns immediately; we just got the interrupt
-
-    // blink(&mut led);
-
-    // // 5 seconds of low-power sleep mode
-    // exti.wait_for_irq(exti_line, pwr.low_power_sleep_mode(&mut scb, &mut rcc));
-    // timer.wait().unwrap(); // returns immediately; we just got the interrupt
-
-    // blink(&mut led);
-
     // 20 seconds of stop mode
     timer.start(20u32);
+    prepare_to_stop(&mut rcc);
     exti.wait_for_irq(
         exti_line,
         pwr.stop_mode(
@@ -110,6 +75,71 @@ fn main() -> ! {
     // reach this point.
     loop {
         blink(&mut rled);
+    }
+}
+
+fn prepare_to_stop(rcc: &mut Rcc) {
+    // We are about to stop, and then reset, so it is ok to break all the rules
+    // and work directly on peripherals that might have been used before.
+    unsafe {
+        let p = pac::Peripherals::steal();
+
+        let spi1 = p.SPI1;
+        let spi2 = p.SPI2;
+
+        // wait for all spi activity to end
+        while spi1.sr.read().bsy().bit_is_set() || spi2.sr.read().bsy().bit_is_set() {}
+
+        let gpiob = p.GPIOB.split(rcc);
+        let gpioa = p.GPIOA.split(rcc);
+
+        let _ = gpiob.pb0.into_analog();
+        let _ = gpiob.pb1.into_analog();
+        let _ = gpiob.pb2.into_analog();
+        let _ = gpiob.pb3.into_analog();
+        // led, leave alone
+        // let _ = gpiob.pb4.into_analog();
+        let _ = gpiob.pb5.into_analog();
+        let _ = gpiob.pb6.into_analog();
+        let _ = gpiob.pb7.into_analog();
+        let _ = gpiob.pb8.into_analog();
+        let _ = gpiob.pb9.into_analog();
+        let _ = gpiob.pb10.into_analog();
+        let _ = gpiob.pb11.into_analog();
+        let _ = gpiob.pb12.into_analog();
+        let _ = gpiob.pb13.into_analog();
+        let _ = gpiob.pb14.into_analog();
+        let _ = gpiob.pb15.into_analog();
+
+        let _ = gpioa.pa0.into_analog();
+        let _ = gpioa.pa1.into_analog();
+        let _ = gpioa.pa2.into_analog();
+        let _ = gpioa.pa3.into_analog();
+        let _ = gpioa.pa4.into_analog();
+        // led, leave alone
+        // let _ = gpioa.pa5.into_analog();
+        let _ = gpioa.pa6.into_analog();
+        let _ = gpioa.pa7.into_analog();
+        let _ = gpioa.pa8.into_analog();
+        let _ = gpioa.pa9.into_analog();
+        let _ = gpioa.pa10.into_analog();
+        let _ = gpioa.pa11.into_analog();
+        let _ = gpioa.pa12.into_analog();
+        // debug pins, leave alone
+        // let _ = gpioa.pa13.into_analog();
+        // let _ = gpioa.pa14.into_analog();
+        let _ = gpioa.pa15.into_analog();
+
+        // Disable all gpio clocks
+        rcc.iopenr.modify(|_, w| w.iopaen().disabled());
+        rcc.iopenr.modify(|_, w| w.iopben().disabled());
+        rcc.iopenr.modify(|_, w| w.iopcen().disabled());
+        rcc.iopenr.modify(|_, w| w.iopden().disabled());
+        rcc.iopenr.modify(|_, w| w.iopeen().disabled());
+        rcc.iopenr.modify(|_, w| w.iophen().disabled());
+
+        // Disable adc clock
+        rcc.apb2enr.modify(|_, w| w.adcen().disabled());
     }
 }
 
